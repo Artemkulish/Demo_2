19430a7ed278cfb5ebe2f02d302fd3acc54b66e9
Artemkulish <57228865+Artemkulish@users.noreply.github.com>
2020-03-22 22:31:24 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,33 +1,20 @@
 #!bin/bash
-
 echo "CHECKING IF PORT 8080 IS OCCUPIED"
-
-sleep 1
-
 netstat -ntulp | grep 8080
 
-sleep 1
-
 echo "KILLING THE PROCESS"
-
 kill -9 `sudo lsof -t -i:8080`
 
 echo "STARTING THE APPLICATION"
-
 nohup java -jar Gateway-0.0.1-SNAPSHOT.jar </dev/null &>/dev/null &
 
-sleep 5
-
-echo "CHECKING ALIVE JAVA PROCESSING"
-
+echo "CHECKING ALIVE JAVA PROCESSES"
 netstat -ntulp | grep java
 
-sleep 10
+sleep 20
 
 echo "CHECKING TOMCAT PORT"
-
 netstat -ntulp | grep 8080
 
-echo "LOAD AVERAGE IS: "
-
+echo "LOAD AVERAGE IS:"
 uptime
